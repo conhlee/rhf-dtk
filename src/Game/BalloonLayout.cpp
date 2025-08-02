406515999de83e07be8c5845fc299f2f705b90d5
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:46:08 +0200

    BalloonLayout: W.I.P. impl

---
+++ src/Game/BalloonLayout.cpp
@@ -0,0 +1,58 @@
+#include "BalloonLayout.hpp"
+
+#include "LayoutManager.hpp"
+
+#include "MessageManager.hpp"
+
+#include <revolution/SC.h>
+
+const char layoutFile_message[] = "message.brlyt";
+
+const char *layoutFileTable[] = { layoutFile_message, NULL };
+
+void CBalloonLayout::fn_800399E4(void) {
+    nw4r::lyt::MultiArcResourceAccessor *resAccessor = gLayoutManager->getUnk38();
+
+    u32 layoutBinSize;
+    void *layoutBin = resAccessor->GetResource(0, layoutFileTable[0], &layoutBinSize);
+
+    buildLayout(layoutBin, resAccessor);
+
+    mBoxParent = getLayout()->GetRootPane()->FindPaneByName("N_message_00", true);
+    mBoxTriangleStraight = getLayout()->GetRootPane()->FindPaneByName("P_triangle_00", true);
+    mBoxTriangleCurve = getLayout()->GetRootPane()->FindPaneByName("P_triangle_01", true);
+    mBoxWindow = getLayout()->GetRootPane()->FindPaneByName("W_message_00", true);
+    mBoxTextbox = static_cast<nw4r::lyt::TextBox *>(
+        getLayout()->GetRootPane()->FindPaneByName("T_message_00", true)
+    );
+    mBoxTextbox->SetTextPositionH(nw4r::lyt::HORIZONTALPOSITION_LEFT);
+
+    mBoxTextTagProcessor = new CMyTagProcessor;
+    mBoxTextbox->SetTagProcessor(mBoxTextTagProcessor);
+
+    mOverlayParent = getLayout()->GetRootPane()->FindPaneByName("N_message_01", true);
+    mOverlayPane = getLayout()->GetRootPane()->FindPaneByName("P_message_01", true);
+    mOverlayTextbox = static_cast<nw4r::lyt::TextBox *>(
+        getLayout()->GetRootPane()->FindPaneByName("T_message_01", true)
+    );
+    mOverlayTextbox->SetTextPositionH(nw4r::lyt::HORIZONTALPOSITION_LEFT);
+
+    mOverlayTextTagProcessor = new CMyTagProcessor;
+    mOverlayTextbox->SetTagProcessor(mOverlayTextTagProcessor);
+
+    gMessageManager->fn_80088474(getLayout()->GetRootPane());
+    
+    fn_801D9B10();
+
+    mBoxPaneAccessIdx = -1;
+    mOverlayPaneAccessIdx = -1;
+
+    mBoxParent->SetTranslate(nw4r::math::VEC2(getScaledX(0.0f), 100.0f));
+    mBoxWindow->SetSize(nw4r::lyt::Size(400.0f, 160.0f));
+    mBoxTextbox->SetSize(nw4r::lyt::Size(400.0f - 60.0f, 160.0f));
+
+    mOverlayPane->SetVisible(false);
+    mOverlayTextbox->SetVisible(false);
+
+    mOverlayParent->SetTranslate(nw4r::math::VEC2(0.0f, 100.0f));
+}
---
+++ src/Game/BalloonLayout.hpp
@@ -0,0 +1,65 @@
+#ifndef GAME_BALLOONLAYOUT_HPP
+#define GAME_BALLOONLAYOUT_HPP
+
+#include <revolution/types.h>
+
+#include "Layout.hpp"
+
+#include "MyTagProcessor.hpp"
+
+#include <revolution/SC.h>
+
+class CBalloonLayout : public CLayout {
+public:
+    virtual ~CBalloonLayout(void);
+    virtual void _10(void);
+    virtual void _14(void);
+
+    CBalloonLayout(void) {
+        setUnk0C(0);
+    }
+
+    void fn_800399E4(void);
+    void fn_80039D58(void);
+    void fn_80039E00(u8 accessIdx, BOOL);
+    void fn_80039E2C(u8 visible);
+    void fn_80039E5C(u8 visible);
+    void fn_80039E78(u8 visible);
+    void fn_80039E94(f32 x, f32 y);
+    void fn_80039F30(f32 x, f32 y, BOOL flipTail);
+    void fn_80039FB0(f32 x, f32 y, BOOL flipTail);
+    void fn_8003A030(f32 x, f32 y);
+    void fn_8003A074(s16 bgColor[4], s16 fgColor[4]);
+    void fn_8003A11C(void);
+    void fn_8003A1D0(
+        u8 accessIdx,
+        f32 pX, f32 pY, f32 sX, f32 sY,
+        bool hasTail, f32 tailX, f32 tailY, bool flipTailH
+    );
+    void fn_8003A418(u8 accessIdx, f32 y, f32 h);
+
+private:
+    f32 getScaledX(f32 x) {
+        if (SCGetAspectRatio() == SC_ASPECT_WIDE) {
+            x *= (608.0f / 832.0f);
+        }
+        return x;
+    }
+
+private:
+    nw4r::lyt::Pane *mBoxParent;
+    nw4r::lyt::Pane *mBoxTriangleStraight;
+    nw4r::lyt::Pane *mBoxTriangleCurve;
+    nw4r::lyt::Pane *mBoxWindow;
+    nw4r::lyt::TextBox *mBoxTextbox;
+    CMyTagProcessor *mBoxTextTagProcessor;
+    s32 mBoxPaneAccessIdx;
+
+    nw4r::lyt::Pane *mOverlayParent;
+    nw4r::lyt::Pane *mOverlayPane;
+    nw4r::lyt::TextBox *mOverlayTextbox;
+    CMyTagProcessor *mOverlayTextTagProcessor;
+    s32 mOverlayPaneAccessIdx;
+};
+
+#endif
