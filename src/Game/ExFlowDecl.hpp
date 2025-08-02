054d4fad3bbc33140b890d3c2daa7a23aecb1346
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:48:34 +0200

    TickFlowDecl & ExFlowDecl improvements

--- src/Game/ExFlowDecl.hpp
+++ src/Game/ExFlowDecl.hpp
@@ -8,29 +8,31 @@
 enum {
     /*   ExFlow opcodes (0x100 to 0x141)  */
 
-    TF_SET_SCENE = 0x100, ///< Set the current scene. Args: scene ID, tickflow *
-    TF_SET_SCENE_VER, ///< Set a scene's version. Args: scene ID, version
-    TF_SCENE_DATA, ///< (Un)load a scene's data (cellanim data & layouts). Arg0: 0 for load, 1 for unload; Args: scene ID
+    TF_SET_SCENE = 0x100,       ///< Set the current scene. Args: scene ID, tickflow *
+    TF_SET_SCENE_VER,           ///< Set a scene's version. Args: scene ID, version
+    TF_SCENE_DATA,              ///< (Un)load a scene's data (cellanim data & layouts). Arg0: 0 for load, 1 for unload; Args: scene ID
     TF_103,
     TF_104,
-    TF_SET_PROLOGUE, ///< Set the prologue. Arg0: is 2P; Args: name, version
-    TF_SET_EPILOGUE, ///< Set the epilogue. Args: name, version
-    TF_SET_EPILOGUE_MSG, ///< Set a epilogue message. Arg0: type: 0 for OK, 1 for Try Again, 2 for Superb; Args: string containing BMG message ID
-    TF_108,
+    TF_SET_PROLOGUE,            ///< Set the prologue. Arg0: is 2P; Args: name, version
+    TF_SET_EPILOGUE,            ///< Set the epilogue. Args: name, version
+    TF_SET_EPILOGUE_MSG,        ///< Set a epilogue message. Arg0: type: 0 for OK, 1 for Try Again, 2 for Superb; Args: mesg ID string
+    TF_SET_GRADING_CAPTION,     ///< Set the grading caption. Args: mesg ID string
     TF_109,
-    TF_FADE, ///< Fade the screen in/out. Arg0: 0 for fade out, 1 for fade in; Args: time is in frames, time (in ticks or frames).
+    TF_FADE,                    ///< Fade the screen in/out. Arg0: 0 for fade out, 1 for fade in; Args: time is in frames, time (in ticks or frames).
     TF_10B,
-    TF_10C,
-    TF_10D,
-    TF_UI_PAUSE, ///< Show/hide the "(-) Pause" graphic. Arg0: 0 for show, 1 for hide
+    TF_UI_SKIP_POS,             ///< Set the "(-) Skip" graphic's position.
+                                ///      - Arg0: position: 0 for top-left, 1 for top-right,
+                                ///                        2 for bottom-left, 3 for bottom-right.
+    TF_UI_SKIP_ANIM,            ///< Play the "(-) Skip" graphic's animation. Args: is practice
+    TF_UI_PAUSE,                ///< Show/hide the "(+) Pause" graphic. Arg0: 0 for show, 1 for hide
     TF_10F,
     TF_110,
     TF_111,
-    TF_112,
-    TF_113,
-    TF_114,
-    TF_115,
-    TF_116,
+    TF_MASK01_VISIBLE,          ///< Set mask01's visibility. Args: 0 for invisible, 1 for visible 
+    TF_MASK01_OPACITY,          ///< Set mask01's opacity. Args: opacity (0-255)
+    TF_MASK01_COLOR,            ///< Set mask01's color. Args: color R (0-255), color G (0-255), color B (0-255)
+    TF_MASK01_LAYER,            ///< Set mask01's layer. Args: layer
+    TF_MASK01_FADE,             ///< Fade mask01's opacity. Arg0: TODO; Args: target opacity, time in ticks
     TF_117,
     TF_118,
     TF_119,
@@ -44,7 +46,7 @@ enum {
     TF_121,
     TF_122,
     TF_123,
-    TF_124,
+    TF_MESG_PANE_SET_TEXT,      ///< Set a message pane's text. Arg0: access index; Args: mesg ID string
     TF_125,
     TF_126,
     TF_127,
@@ -52,9 +54,11 @@ enum {
     TF_129,
     TF_12A,
     TF_12B,
-    TF_LOAD_GROUP, ///< Start loading a sound group. Arg0: type (1 for music, 2 for sfx); Args: RSID of group
-    TF_UNLOAD_GROUPS, ///< Unload all groups of a specific type. Arg0: type (1 for music, 2 for sfx)
-    TF_12E,
+    TF_LOAD_SND_GROUP,          ///< Start loading a sound group. Arg0: type (1 for music, 2 for sfx); Args: SID of group
+    TF_UNLOAD_SND_GROUPS,       ///< Unload all groups of a specific type. Arg0: type (1 for music, 2 for sfx)
+    TF_RESET_SND_GROUP_HEAP,    ///< Reset a sound group's heap. Arg0: type (1 for music, 2 for sfx)
+                                ///      - Note: this doesn't unload any previously loaded data, but it will be
+                                ///              overwritten by newly allocated data.
     TF_12F,
     TF_130,
     TF_131,
@@ -71,22 +75,51 @@ enum {
     TF_13C,
     TF_13D,
     TF_13E,
-    TF_REMIX8_MASK_MANAGE, ///< Manage the Remix 8 mask. Arg0: operation (0: load, 1: unload, 2: enquire ready (sets condvar), 3: init, 4: set visible, 5: set layer); Args: is visible / layer
-    TF_ENDLESS_MANAGE, ///< Manage Endless Remix. Arg0: operation (0: load, 1: unload, 2: enquire ready (sets condvar), 3: init, 4: get random scene ID (sets condvar))
+    TF_REMIX8_MASK_MANAGE,      ///< Manage the Remix 8 mask. Arg0: operation (0: load, 1: unload, 2: enquire ready (sets condvar), 3: init, 4: set visible, 5: set layer); Args: is visible / layer
+    TF_ENDLESS_MANAGE,          ///< Manage Endless Remix. Arg0: operation (0: load, 1: unload, 2: enquire ready (sets condvar), 3: init, 4: get random scene ID (sets condvar))
     TF_141
 };
 
 // command macros
 
-#define TFC_SET_SCENE(scene_id, tf_label) TFD_CMD(TF_SET_SCENE, 2, 0), (u32)(scene_id), TFD_PTR(tf_label),
-#define TFC_100_1() TFD_CMD(0x100, 0, 1), // TODO: rename
+#define TFC_SET_SCENE(sceneId, tfLabel) TFD_CMD(TF_SET_SCENE, 2, 0), (TickFlowCode)(sceneId), TFD_PTR(tfLabel),
+#define TFC_100_1() TFD_CMD(TF_SET_SCENE, 0, 1), // TODO: rename
 
-#define TFC_SET_SCENE_VER(scene_id, ver) TFD_CMD(TF_SET_SCENE_VER, 2, 0), (u32)(scene_id), (u32)(ver),
+#define TFC_SET_SCENE_VER(sceneId, ver) TFD_CMD(TF_SET_SCENE_VER, 2, 0), (TickFlowCode)(sceneId), (TickFlowCode)(ver),
 
-#define TFC_SET_EPILOGUE(name_str, ver) TFD_CMD(TF_SET_EPILOGUE, 2, 0), TFD_PTR(name_str), (u32)(ver),
+#define TFC_SET_EPILOGUE(nameStr, ver) TFD_CMD(TF_SET_EPILOGUE, 2, 0), TFD_PTR(nameStr), (TickFlowCode)(ver),
 
-#define TFC_SET_EPILOGUE_MSG_OK(msgid_str) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 0), TFD_PTR(msgid_str),
-#define TFC_SET_EPILOGUE_MSG_TRYAGAIN(msgid_str) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 1), TFD_PTR(msgid_str),
-#define TFC_SET_EPILOGUE_MSG_SUPERB(msgid_str) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 2), TFD_PTR(msgid_str),
+#define TFC_SET_EPILOGUE_MSG_OK(mesgIdStr) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 0), TFD_PTR(mesgIdStr),
+#define TFC_SET_EPILOGUE_MSG_TRYAGAIN(mesgIdStr) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 1), TFD_PTR(mesgIdStr),
+#define TFC_SET_EPILOGUE_MSG_SUPERB(mesgIdStr) TFD_CMD(TF_SET_EPILOGUE_MSG, 1, 2), TFD_PTR(mesgIdStr),
+
+#define TFC_SET_GRADING_CAPTION(mesgIdStr) TFD_CMD(TF_SET_GRADING_CAPTION, 1, 0), TFD_PTR(mesgIdStr),
+
+#define TFC_FADE_IN_FRAMES(frames) TFD_CMD(TF_FADE, 2, 1), (TickFlowCode)(1), (TickFlowCode)(frames),
+#define TFC_FADE_OUT_FRAMES(frames) TFD_CMD(TF_FADE, 2, 0), (TickFlowCode)(1), (TickFlowCode)(frames),
+
+#define TFC_FADE_IN_TICKS(frames) TFD_CMD(TF_FADE, 2, 1), (TickFlowCode)(0), (TickFlowCode)(frames),
+#define TFC_FADE_OUT_TICKS(frames) TFD_CMD(TF_FADE, 2, 0), (TickFlowCode)(0), (TickFlowCode)(frames),
+
+#define TFC_UI_SKIP_POS(pos) TFD_CMD(TF_UI_SKIP_POS, 0, pos),
+
+#define TFC_UI_SKIP_ANIM() TFD_CMD(TF_UI_SKIP_ANIM, 1, 0), (TickFlowCode)(0),
+#define TFC_UI_SKIP_ANIM_PRACTICE() TFD_CMD(TF_UI_SKIP_ANIM, 1, 0), (TickFlowCode)(1),
+
+#define TFC_MASK01_VISIBLE(isVisible) TFD_CMD(TF_MASK01_VISIBLE, 1, 0), (TickFlowCode)((isVisible) ? 1 : 0),
+#define TFC_MASK01_OPACITY(opacity) TFD_CMD(TF_MASK01_OPACITY, 1, 0), (TickFlowCode)(opacity),
+#define TFC_MASK01_COLOR(r, g, b) TFD_CMD(TF_MASK01_COLOR, 3, 0), (TickFlowCode)(r), (TickFlowCode)(g), (TickFlowCode)(b),
+#define TFC_MASK01_LAYER(layer) TFD_CMD(TF_MASK01_LAYER, 1, 0), (TickFlowCode)(layer),
+#define TFC_MASK01_FADE(targetOpacity, ticks) TFD_CMD(TF_MASK01_FADE, 2, 1), (TickFlowCode)(targetOpacity), (TickFlowCode)(ticks),
+
+#define TFC_RESET_SND_GROUP_HEAP(type) TFD_CMD(TF_RESET_SND_GROUP_HEAP, 0, type),
+
+#define TFC_MESG_PANE_SET_TEXT(accessIdx, mesgIdStr) TFD_CMD(TF_MESG_PANE_SET_TEXT, 1, accessIdx), TFD_PTR(mesgIdStr),
+
+// TODO
+#define TFC_11A(value) TFD_CMD(TF_11A, 1, 0), (TickFlowCode)(value),
+
+// TODO
+#define TFC_11D() TFD_CMD(TF_11D, 0, 0),
 
 #endif
--- src/GameUtil/TickFlowDecl.hpp
+++ src/GameUtil/TickFlowDecl.hpp
@@ -8,10 +8,10 @@ typedef u32 TickFlowCode;
 enum {
     /*   TickFlow opcodes (0x000 to 0x050)   */
 
-    TF_ASYNC_CALL = 0x000, ///< Args: tickflow *, delay in ticks
-    TF_CALL, ///< Args: tickflow *
-    TF_RETURN,
-    TF_STOP,
+    TF_ASYNC_CALL = 0x000, ///< Args: TickFlowCode *, delay in ticks
+    TF_CALL, ///< Args: TickFlowCode *
+    TF_RETURN, // Return to the previous call.
+    TF_STOP, // End execution.
     TF_CATEGORY, ///< Set category. Args: category to set
     TF_SET_CONDVAR, ///< Args: value to set condvar to
     TF_INCR_CONDVAR, ///< Args: value to increment condvar by
@@ -19,12 +19,12 @@ enum {
     TF_POP_CONDVAR,
     TF_REST, ///< Arg0: ticks to rest for
     TF_00A,
-    TF_00B,
+    TF_REST_FRAMES, ///< Arg0: frames to rest for
     TF_UNREST, ///< Arg0: ticks to decrement the current rest by
     TF_00D,
     TF_LABEL, ///< Arg0: label identifier
     TF_JUMP, ///< Arg0: identifier of label to jump to
-    TF_IF, ///< Multiple variations depending on arg0. Args: value to compare to condvar
+    TF_IF, ///< TODO Multiple variations depending on arg0. Args: value to compare to condvar
     TF_ELSE,
     TF_ENDIF,
     TF_SWITCH_BEGIN,
@@ -35,15 +35,15 @@ enum {
     TF_018,
     TF_019,
     TF_TEMPO, ///< Set tempo. Arg0: BPM to set
-    TF_TEMPO_SEQ, ///< Set tempo. Arg0: RSID of sequence to reference BPM from
-    TF_TEMPO_WAVE, ///< Set tempo. Arg0: RSID of wave to reference BPM from
+    TF_TEMPO_SEQ, ///< Set tempo. Arg0: SID of sequence to reference BPM from
+    TF_TEMPO_WAVE, ///< Set tempo. Arg0: SID of wave to reference BPM from
     TF_SPEED, ///< Set speed. Arg0: Speed value (fixed, 256)
     TF_01E,
     TF_01F,
     TF_020,
     TF_SPAWN_CELLANIM, ///< Create and display a new CellAnim. Arg0: TODO; Args: cell ID, anim ID, pos X, pos Y, display layer, scale X, scale Y
-    TF_PLAY_SFX, ///< Play a SFX. Args: RSID of SFX, volume (fixed, 256)
-    TF_PLAY_SFX_EX, ///< Play a SFX. Arg0: TODO; Args: RSID of SFX, volume, pitch, pan, (UNUSED), start offset (all args are fixed, 256)
+    TF_PLAY_SFX_VOL, ///< Play a SFX. Args: SID of SFX, volume (fixed, 256)
+    TF_PLAY_SFX, ///< Play a SFX. Arg0: TODO; Args: SID of SFX, volume, pitch, pan, (UNUSED), start offset (all args are fixed, 256)
     TF_024,
     TF_025,
     TF_026,
@@ -63,7 +63,7 @@ enum {
     TF_034,
     TF_035,
     TF_036,
-    TF_PREPARE_WAVE, ///< Prepare WSD (wave sound data) for playback. Arg0: RSID of WSD
+    TF_PREPARE_WAVE, ///< Prepare WSD (wave sound data) for playback. Arg0: SID of WSD
     TF_038,
     TF_PLAY_WAVE, ///< Start WSD (wave sound data) playback.
     TF_03A,
@@ -72,7 +72,7 @@ enum {
     TF_03D,
     TF_03E,
     TF_GET_GROUP_LOADING, ///< Set condvar to 1 if the current group is loading, and 0 if not.
-    TF_040,
+    TF_MESG_PANE_VISIBLE, ///< Set a message pane's visibillity. Arg0: access index; Args: is visible
     TF_041,
     TF_042,
     TF_043,
@@ -81,14 +81,13 @@ enum {
     TF_046,
     TF_047,
     TF_048,
-    TF_SET_TUTORIAL_SKIPPABLE, ///< Set if the player is allowed to skip the tutorial. Arg0: enabled
-    TF_049,
-    TF_04A,
+    TF_SET_SKIPPABLE, ///< Set if the player is allowed to skip. Arg0: enabled
+    TF_SET_SKIP_HANDLER, ///< Set the TickFlow that runs when a skip is triggered. Args: TickFlowCode *
     TF_04B,
     TF_04C,
     TF_04D,
     TF_04E,
-    TF_RANDOM, ///< Set the condvar to a random value. Arg0: maximum value
+    TF_RANDOM, ///< Set the condvar to a random value. Arg0: upper bound of random value
 
     /*   ExFlow opcodes (0x100 to 0x141) are found in Game/ExFlowDecl.hpp */
 };
@@ -119,6 +118,40 @@ enum {
 
 // command macros
 
-#define TFC_CALL(tf_label) TFD_CMD(TF_CALL, 1, 0), TFD_PTR(tf_label),
+#define TFC_ASYNC_CALL(tfLabel, delay) TFD_CMD(TF_ASYNC_CALL, 2, 0), TFD_PTR(tfLabel), (TickFlowCode)(delay),
+#define TFC_CALL(tfLabel) TFD_CMD(TF_CALL, 1, 0), TFD_PTR(tfLabel),
+
+#define TFC_CATEGORY(category) TFD_CMD(TF_CATEGORY, 1, 0), (TickFlowCode)(category),
+
+#define TFC_REST(ticks) TFD_CMD(TF_REST, 0, ticks),
+#define TFC_REST_FRAMES(frames) TFD_CMD(TF_REST_FRAMES, 0, frames),
+
+#define TFC_IF_EQU(value) TFD_CMD(TF_IF, 1, 0), (TickFlowCode)(value),
+#define TFC_IF_NEQ(value) TFD_CMD(TF_IF, 1, 1), (TickFlowCode)(value),
+#define TFC_IF_LT(value) TFD_CMD(TF_IF, 1, 2), (TickFlowCode)(value),
+#define TFC_IF_LEQ(value) TFD_CMD(TF_IF, 1, 3), (TickFlowCode)(value),
+#define TFC_IF_GT(value) TFD_CMD(TF_IF, 1, 4), (TickFlowCode)(value),
+#define TFC_IF_GEQ(value) TFD_CMD(TF_IF, 1, 5), (TickFlowCode)(value),
+
+#define TFC_ELSE() TFD_CMD(TF_ELSE, 0, 0),
+
+#define TFC_ENDIF(value) TFD_CMD(TF_ENDIF, 0, 0),
+
+#define TFC_TEMPO(tempo) TFD_CMD(TF_TEMPO, 0, tempo),
+
+#define TFC_TEMPO_WAVE(sid) TFD_CMD(TF_TEMPO_WAVE, 0, sid),
+
+#define TFC_SPEED(speed) TFD_CMD(TF_SPEED, 0, speed),
+
+#define TFC_PLAY_SFX(sid) TFD_CMD(TF_PLAY_SFX, 1, 0), (TickFlowCode)(sid),
+
+#define TFC_PREPARE_WAVE(sid) TFD_CMD(TF_PREPARE_WAVE, 0, sid),
+#define TFC_PLAY_WAVE() TFD_CMD(TF_PLAY_WAVE, 0, 0),
+
+#define TFC_MESG_PANE_VISIBLE(accessIdx, isVisible) TFD_CMD(TF_MESG_PANE_VISIBLE, 1, accessIdx), (TickFlowCode)((isVisible) ? 1 : 0),
+
+#define TFC_SET_SKIPPABLE(isSkippable) TFD_CMD(TF_SET_SKIPPABLE, 0, (isSkippable) ? 1 : 0),
+
+#define TFC_SET_SKIP_HANDLER(tfLabel) TFD_CMD(TF_SET_SKIP_HANDLER, 1, 0), TFD_PTR(tfLabel),
 
 #endif
