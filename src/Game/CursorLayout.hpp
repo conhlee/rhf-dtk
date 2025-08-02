bf7a5f057b4c6fde6ad09d63ea045318cc6dfedc
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:46:18 +0200

    CursorLayout header

---
+++ src/Game/CursorLayout.hpp
@@ -0,0 +1,21 @@
+#ifndef GAME_CURSORLAYOUT_HPP
+#define GAME_CURSORLAYOUT_HPP
+
+#include <revolution/types.h>
+
+#include "Layout.hpp"
+
+class CCursorLayout : public CLayout {
+public:
+    virtual ~CCursorLayout(void);
+    virtual void _10(void);
+
+    CCursorLayout(void) {
+        setUnk0C(0);
+    }
+
+private:
+    u8 pad[0x3C - 0x1C]; // temp
+};
+
+#endif
