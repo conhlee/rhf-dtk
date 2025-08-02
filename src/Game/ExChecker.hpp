3646394672a9f144455b419ee7d77981560011ed
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:46:25 +0200

    ExChecker header

---
+++ src/Game/ExChecker.hpp
@@ -0,0 +1,23 @@
+#ifndef GAME_EXCHECKER_HPP
+#define GAME_EXCHECKER_HPP
+
+#include <revolution/types.h>
+
+#include "InputChecker.hpp"
+
+class CExChecker : public CInputChecker {
+public:
+    virtual ~CExChecker(void) {};
+    virtual void _14(void);
+    virtual u32 _18(void);
+    virtual u32 _20(u8 *, u32, f32);
+
+    CExChecker(void) {
+        fn_8008EB08(0);
+    }
+
+    void fn_8008EB08(u8);
+};
+
+
+#endif
