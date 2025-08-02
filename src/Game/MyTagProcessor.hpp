ac348a5671a004b20716f4477545ca2e33054f33
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:40:38 +0200

    MyTagProcessor header

---
+++ src/Game/MyTagProcessor.hpp
@@ -0,0 +1,26 @@
+#ifndef GAME_MYTAGPROCESSOR_HPP
+#define GAME_MYTAGPROCESSOR_HPP
+
+#include <revolution/types.h>
+
+#include <nw4r/ut.h>
+
+class CMyTagProcessor : public nw4r::ut::WideTagProcessor {
+public:
+    virtual ~CMyTagProcessor(void);
+
+    virtual Operation Process(u16 ch, ContextType* pCtx);
+    virtual Operation CalcRect(nw4r::ut::Rect* pRect, u16 ch,
+                               ContextType* pCtx);
+
+    CMyTagProcessor(void);
+
+private:
+    f32 mRubyScale;
+    nw4r::ut::Color mGradationStartColor;
+    nw4r::ut::Color mGradationEndColor;
+    nw4r::ut::Color mColorMappingMin;
+    nw4r::ut::Color mColorMappingMax;
+};
+
+#endif
