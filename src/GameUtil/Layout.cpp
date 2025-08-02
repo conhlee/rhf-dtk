c21391ef98935db6236e3247c4d5c1d8c2eb27d1
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:37:55 +0200

    Layout: W.I.P. implementation

--- src/GameUtil/Layout.cpp
+++ src/GameUtil/Layout.cpp
@@ -1,9 +1,88 @@
 #include "Layout.hpp"
 
-// TODO: functions above the clayout ctor
+#include "Mem.hpp"
+
+#include "LayoutManager.hpp"
 
 CLayout::CLayout(void) {
-
+    mAnimationCount = 0;
+    mLayout = NULL;
+    mAnimations = NULL;
 }
 
-// TODO: functions below the clayout ctor
+void CLayout::_0C(void) {
+    mLayout = new nw4r::lyt::Layout;
+
+    mAnimations = new CLayoutAnimation[mAnimationCount];
+
+    mUnk10 = 1;
+    mUnk11 = 1;
+}
+
+void CLayout::fn_801D9B10(void) {
+    mUnk12 = 1;
+    mScaleX = 1.0f;
+    mScaleY = 1.0f;
+}
+
+void CLayout::_14(void) {
+    if (mLayout != NULL) {
+        delete mLayout;
+    }
+    if (mAnimations != NULL) {
+        delete[] mAnimations;
+    }
+}
+
+void CLayout::_18(nw4r::lyt::DrawInfo *drawInfo) {
+    f32 scaleX = mScaleX;
+    f32 scaleY = mScaleY;
+
+    if (mUnk12 != 0) {
+        scaleX *= gLayoutManager->fn_801D7188();
+        scaleY *= gLayoutManager->fn_801D7190();
+    }
+
+    mLayout->GetRootPane()->SetScale(nw4r::math::VEC2(scaleX, scaleY));
+
+    _20(drawInfo);
+
+    for (s32 i = 0; i < mAnimationCount; i++) {
+        mAnimations[i].fn_801DA354();
+    }
+
+    mLayout->Animate(0);
+    mLayout->CalculateMtx(*drawInfo);
+
+    for (s32 i = 0; i < mAnimationCount; i++) {
+        mAnimations[i].fn_801DA590();
+    }
+}
+
+void CLayout::_1C(nw4r::lyt::DrawInfo *drawInfo) {
+    _20(drawInfo);
+
+    mLayout->Draw(*drawInfo);
+}
+
+void CLayout::_20(nw4r::lyt::DrawInfo *drawInfo) {
+    nw4r::ut::Rect viewRect = mLayout->GetLayoutRect();
+
+    f32 nearField = 0.0f;
+    f32 farField = 500.0f;
+    
+    Mtx44 projMtx;
+    C_MTXOrtho(projMtx, viewRect.top, viewRect.bottom, viewRect.left, viewRect.right, nearField, farField);
+
+    GXSetProjection(projMtx, GX_ORTHOGRAPHIC);
+
+    nw4r::math::MTX34 viewMtx;
+    PSMTXIdentity(viewMtx.mtx);
+
+    drawInfo->SetViewMtx(viewMtx);
+    drawInfo->SetViewRect(viewRect);
+}
+
+void CLayout::fn_801D9E24(bool enable) {
+    mUnk12 = enable;
+}
--- src/GameUtil/Layout.hpp
+++ src/GameUtil/Layout.hpp
@@ -2,52 +2,62 @@
 #define GAMEUTIL_CLAYOUT_HPP
 
 #include <revolution/types.h>
-#include <nw4r/lyt/layout.h>
-#include <nw4r/lyt/drawInfo.h>
+
+#include <nw4r/lyt.h>
+
 #include "LayoutAnimation.hpp"
 
 class CLayout {
 public:
-
-    virtual ~CLayout(void) {
-
-    }
+    virtual ~CLayout(void) {}
     virtual void _0C(void);
-    virtual void _10(void);
+    virtual void _10(void) {
+        fn_801D9B10();
+    }
     virtual void _14(void);
     virtual void _18(nw4r::lyt::DrawInfo *);
     virtual void _1C(nw4r::lyt::DrawInfo *);
-    virtual void _20(void);
-    
+    virtual void _20(nw4r::lyt::DrawInfo *);
+
     CLayout(void);
 
     void fn_801D9B10(void);
 
+    void fn_801D9E24(bool);
+
     nw4r::lyt::Layout *getLayout(void) {
-        return unk04;
+        return mLayout;
     }
+
+    CLayoutAnimation *getAnimations(void) {
+        return mAnimations;
+    }
+    CLayoutAnimation *getAnimation(u32 index) {
+        return &mAnimations[index];
+    }
+
     void buildLayout(const void *buf, nw4r::lyt::ResourceAccessor *resAccessor) {
         getLayout()->Build(buf, resAccessor);
     }
     void setUnk0C(s32 arg0) {
-        unk0C = arg0;
+        mAnimationCount = arg0;
     }
     u8 getUnk10(void) {
-        return unk10;
+        return mUnk10;
     }
-    u8 getUnk11(void) {
-        return unk11;
-    }
-private:
 
-    nw4r::lyt::Layout *unk04;
-    CLayoutAnimation *unk08;
-    s32 unk0C;
-    u8 unk10;
-    u8 unk11;
-    u8 unk12;
-    f32 unk14;
-    f32 unk18;
+    u8 getUnk11(void) { return mUnk11; }
+    void setUnk11(u8 unk) { mUnk11 = unk; }
+
+private:
+    nw4r::lyt::Layout *mLayout;
+    CLayoutAnimation *mAnimations; // CLayoutAnimation[mAnimationCount]
+    s32 mAnimationCount;
+    u8 mUnk10;
+    u8 mUnk11;
+    u8 mUnk12;
+    f32 mScaleX;
+    f32 mScaleY;
 };
 
 #endif
