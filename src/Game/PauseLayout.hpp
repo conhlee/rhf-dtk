eb8737c3fd01167fc76999e4fca11f532309f0d4
Angelo <48489278+conhlee@users.noreply.github.com>
2025-08-02 19:46:47 +0200

    PauseLayout header

---
+++ src/Game/PauseLayout.hpp
@@ -0,0 +1,25 @@
+#ifndef GAME_PAUSELAYOUT_HPP
+#define GAME_PAUSELAYOUT_HPP
+
+#include <revolution/types.h>
+
+#include "Layout.hpp"
+
+class CPauseLayout : public CLayout {
+public:
+    virtual ~CPauseLayout(void);
+    virtual void _10(void);
+
+    CPauseLayout(void) {
+        setUnk0C(3);
+    }
+
+    void setUnk1D(u8 unk) { mUnk1D = unk; }
+
+private:
+    u8 mUnk1C;
+    u8 mUnk1D;
+    u8 pad[0x2C - 0x1C - 2]; // temp
+};
+
+#endif
