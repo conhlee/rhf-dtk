f1b017be4cf8f7a078e2d04b69254d7731ed05a7
Angelo <48489278+conhlee@users.noreply.github.com>
2025-09-28 23:53:05 +0200

    Psuedo3D: name function args in header

--- src/Game/Pseudo3D.hpp
+++ src/Game/Pseudo3D.hpp
@@ -14,8 +14,8 @@ public:
 
     CPseudo3D(void);
 
-    void fn_80072A14(f32, f32, f32, f32, f32, f32);
-    void fn_80072A54(f32, f32, f32, f32 *, f32 *, f32 *);
+    void fn_80072A14(f32 centerX, f32 centerY, f32 scale, f32 focX, f32 focY, f32 camZ);
+    void fn_80072A54(f32 x, f32 y, f32 z, f32 *xOut, f32 *yOut, f32 *scaleOut);
 };
 
 #endif
