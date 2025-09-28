3876b43e4dadefd2953dbf4fa9b5daa470eee4e7
Angelo <48489278+conhlee@users.noreply.github.com>
2025-09-28 23:47:33 +0200

    Mem: refactoring & EHeapGroup

--- src/GameUtil/Mem.cpp
+++ src/GameUtil/Mem.cpp
@@ -3,32 +3,30 @@
 MEMiHeapHead *lbl_80320F80; // gHeapMEM1
 MEMiHeapHead *lbl_80320F84; // gHeapMEM2
 static bool lbl_80320F88; // sHeapInitialized
-static u8 lbl_80320F89; 
 
-static u16 lbl_803D5C28[16];
+static u8 lbl_80320F89; // sHeapGroupIDStackPos
+static u16 lbl_803D5C28[16]; // sHeapGroupIDStack
 
-void *fn_801D3784(size_t, EHeapMEM, s32); // doAlloc
-void fn_801D3568(void); // initHeap
+static void *fn_801D3784(size_t, EHeapMEM, s32); // doAlloc
 
 void *operator new(size_t size) {
-    void *object;
-    BOOL interrupt;
-
     if (!lbl_80320F88) {
         fn_801D3568();
     }
 
-    interrupt = OSDisableInterrupts();
-    object = MEMAllocFromExpHeap(lbl_80320F84, size);
+    BOOL interrupt = OSDisableInterrupts();
+    void *alloc = MEMAllocFromExpHeap(lbl_80320F84, size);
     OSRestoreInterrupts(interrupt);
-    if (object == NULL) {
+
+    if (alloc == NULL) {
         OSReport("Can't Alloc Heap\n");
         OSReport(" Required : %d B\n", size);
         OSReport(" Free     : %d B\n", MEMGetTotalFreeSizeForExpHeap(lbl_80320F84));
         OSReport(" Max Size : %d B\n", MEMGetAllocatableSizeForExpHeap(lbl_80320F84));
         OSReport(" Total    : %d B\n", (u8 *)lbl_80320F84->end - (u8 *)lbl_80320F84);
     }
-    return object;
+
+    return alloc;
 }
 
 void *operator new(size_t size, EHeapMEM heap, s32 align) {
@@ -36,24 +34,23 @@ void *operator new(size_t size, EHeapMEM heap, s32 align) {
 }
 
 void *operator new[](size_t size) {
-    void *object;
-    BOOL interrupt;
-
     if (!lbl_80320F88) {
         fn_801D3568();
     }
 
-    interrupt = OSDisableInterrupts();
-    object = MEMAllocFromExpHeap(lbl_80320F84, size);
+    BOOL interrupt = OSDisableInterrupts();
+    void *alloc = MEMAllocFromExpHeap(lbl_80320F84, size);
     OSRestoreInterrupts(interrupt);
-    if (object == NULL) {
+
+    if (alloc == NULL) {
         OSReport("Can't Alloc Heap\n");
         OSReport(" Required : %d B\n", size);
         OSReport(" Free     : %d B\n", MEMGetTotalFreeSizeForExpHeap(lbl_80320F84));
         OSReport(" Max Size : %d B\n", MEMGetAllocatableSizeForExpHeap(lbl_80320F84));
         OSReport(" Total    : %d B\n", (u8 *)lbl_80320F84->end - (u8 *)lbl_80320F84);
     }
-    return object;
+
+    return alloc;
 }
 
 void *operator new[](size_t size, EHeapMEM heap, s32 align) {
@@ -61,137 +58,151 @@ void *operator new[](size_t size, EHeapMEM heap, s32 align) {
 }
 
 void operator delete(void *mem) {
-    BOOL interrupt;
+    BOOL interrupt = OSDisableInterrupts();
 
-    interrupt = OSDisableInterrupts();
     if (mem != NULL) {
         MEMiHeapHead *found = MEMFindContainHeap(mem);
         if (found == lbl_80320F80) {
             MEMFreeToExpHeap(lbl_80320F80, mem);
-        } else if (found == lbl_80320F84) {
+        }
+        else if (found == lbl_80320F84) {
             MEMFreeToExpHeap(lbl_80320F84, mem);
         }
     }
+
     OSRestoreInterrupts(interrupt);
 }
 
 void operator delete[](void *mem) {
-    BOOL interrupt;
+    BOOL interrupt = OSDisableInterrupts();
 
-    interrupt = OSDisableInterrupts();
     if (mem != NULL) {
         MEMiHeapHead *found = MEMFindContainHeap(mem);
         if (found == lbl_80320F80) {
             MEMFreeToExpHeap(lbl_80320F80, mem);
-        } else if (found == lbl_80320F84) {
+        }
+        else if (found == lbl_80320F84) {
             MEMFreeToExpHeap(lbl_80320F84, mem);
         }
     }
+
     OSRestoreInterrupts(interrupt);
 }
 
-void fn_801D3564(void) {
-
-}
+void fn_801D3564(void) {}
 
 // Initialize the heaps
 void fn_801D3568(void) {
     if (!lbl_80320F88) {
         void *arena1Lo = OSGetMEM1ArenaLo();
         void *arena1Hi = OSGetMEM1ArenaHi();
-        lbl_80320F80 = MEMCreateExpHeapEx(arena1Lo, (u8 *)arena1Hi - (u8 *)arena1Lo, 0);
+        u32 arena1Size = (u8 *)arena1Hi - (u8 *)arena1Lo;
+
+        lbl_80320F80 = MEMCreateExpHeap(arena1Lo, arena1Size);
         OSSetMEM1ArenaLo(arena1Hi);
 
         void *arena2Lo = OSGetMEM2ArenaLo();
         void *arena2Hi = OSGetMEM2ArenaHi();
-        u32 size = (u8 *)arena2Hi - (u8 *)arena2Lo;
-        if (size > 0x4000000) {
-            size -= 0x4000000;
-            arena2Hi = (void *)((u8 *)arena2Hi - 0x4000000);
+        u32 arena2Size = (u8 *)arena2Hi - (u8 *)arena2Lo;
+
+        // NDEV units had a 128MB MEM2 size, so if the size of MEM2
+        // exceeds the size of MEM2 on retail, reduce it to match.
+        if (arena2Size > 0x4000000) {
+            arena2Size -= 0x4000000;
+            arena2Hi = (u8 *)arena2Hi - 0x4000000;
         }
-        lbl_80320F84 = MEMCreateExpHeapEx(arena2Lo, size, 0);
+
+        lbl_80320F84 = MEMCreateExpHeap(arena2Lo, arena2Size);
         OSSetMEM2ArenaLo(arena2Hi);
-        lbl_80320F88 = 1;
+
+        lbl_80320F88 = true;
+
         lbl_80320F89 = 0;
         MEMSetGroupIDForExpHeap(lbl_80320F80, 0);
         MEMSetGroupIDForExpHeap(lbl_80320F84, 0);
     }
 }
 
-void fn_801D3634(void) {
+void fn_801D3634(void) {}
 
-}
-
-void fn_801D3638(u32 arg0) {
-
-}
+void fn_801D3638(u32) {}
 
 u16 fn_801D363C(void) {
     return MEMGetGroupIDForExpHeap(lbl_80320F80);
 }
 
 void fn_801D3644(void) {
-    u32 id;
-
-    id = lbl_803D5C28[--lbl_80320F89];
+    u16 id = lbl_803D5C28[--lbl_80320F89];
     MEMSetGroupIDForExpHeap(lbl_80320F80, id);
     MEMSetGroupIDForExpHeap(lbl_80320F84, id);
 }
 
-void fn_801D369C(u16 id) {
+void fn_801D369C(u16 groupID) {
     lbl_803D5C28[lbl_80320F89++] = MEMGetGroupIDForExpHeap(lbl_80320F80);
-    MEMSetGroupIDForExpHeap(lbl_80320F80, id);
-    MEMSetGroupIDForExpHeap(lbl_80320F84, id);
+    MEMSetGroupIDForExpHeap(lbl_80320F80, groupID);
+    MEMSetGroupIDForExpHeap(lbl_80320F84, groupID);
 }
 
-void fn_801D3700(void *mem, MEMiHeapHead *heap, u32 id) {
-    if (id == MEMGetGroupIDForMBlockExpHeap(mem)) {
+static void fn_801D3700(void *mem, MEMiHeapHead *heap, u32 groupID) {
+    if (groupID == MEMGetGroupIDForMBlockExpHeap(mem)) {
         MEMFreeToExpHeap(heap, mem);
     }
 }
 
-void fn_801D375C(u32 id) {
-    MEMVisitAllocatedForExpHeap(lbl_80320F80, fn_801D3700, id);
+void fn_801D375C(u16 groupID) {
+    MEMVisitAllocatedForExpHeap(lbl_80320F80, fn_801D3700, groupID);
 }
 
-void fn_801D3770(u32 id) {
-    MEMVisitAllocatedForExpHeap(lbl_80320F84, fn_801D3700, id);
+void fn_801D3770(u16 groupID) {
+    MEMVisitAllocatedForExpHeap(lbl_80320F84, fn_801D3700, groupID);
 }
 
-void *fn_801D3784(size_t size, EHeapMEM heap, s32 align) {
-    void *object;
-    BOOL interrupt;
-
+static void *fn_801D3784(size_t size, EHeapMEM heap, s32 align) {
     if (!lbl_80320F88) {
         fn_801D3568();
     }
 
-    interrupt = OSDisableInterrupts();
-    object = NULL;
+    BOOL interrupt = OSDisableInterrupts();
 
+    void *alloc = NULL;
     if (heap == eHeap_MEM1) {
         if (align == 0) {
-            object = MEMAllocFromExpHeap(lbl_80320F80, size);
-        } else {
-            object = MEMAllocFromExpHeapEx(lbl_80320F80, size, align);
+            alloc = MEMAllocFromExpHeap(lbl_80320F80, size);
         }
-    } else if (heap == eHeap_MEM2) {
-        if (align == 0) {
-            object = MEMAllocFromExpHeap(lbl_80320F84, size);
-        } else {
-            object = MEMAllocFromExpHeapEx(lbl_80320F84, size, align);
+        else {
+            alloc = MEMAllocFromExpHeapEx(lbl_80320F80, size, align);
         }
     }
+    else if (heap == eHeap_MEM2) {
+        if (align == 0) {
+            alloc = MEMAllocFromExpHeap(lbl_80320F84, size);
+        }
+        else {
+            alloc = MEMAllocFromExpHeapEx(lbl_80320F84, size, align);
+        }
+    }
+
     OSRestoreInterrupts(interrupt);
-    if (object == NULL) {
-        OSReport("Can\'t Alloc Heap\n");
+
+    if (alloc == NULL) {
+        OSReport("Can't Alloc Heap\n");
         OSReport(" Required : %d B\n", size);
         OSReport(" Free     : %d B\n", 
-            (heap == eHeap_MEM1) ? MEMGetTotalFreeSizeForExpHeap(lbl_80320F80) : MEMGetTotalFreeSizeForExpHeap(lbl_80320F84));
+            (heap == eHeap_MEM1) ?
+                MEMGetTotalFreeSizeForExpHeap(lbl_80320F80) :
+                MEMGetTotalFreeSizeForExpHeap(lbl_80320F84)
+        );
         OSReport(" Max Size : %d B\n", 
-            (heap == eHeap_MEM1) ? MEMGetAllocatableSizeForExpHeap(lbl_80320F80) : MEMGetAllocatableSizeForExpHeap(lbl_80320F84));
+            (heap == eHeap_MEM1) ?
+                MEMGetAllocatableSizeForExpHeap(lbl_80320F80) :
+                MEMGetAllocatableSizeForExpHeap(lbl_80320F84)
+        );
         OSReport(" Total    : %d B\n",
-            (heap == eHeap_MEM1) ? (u32)(lbl_80320F80->end - (u32)lbl_80320F80) : (u32)(lbl_80320F84->end - (u32)lbl_80320F84));
+            (heap == eHeap_MEM1) ?
+                (u32)(lbl_80320F80->end - (u8 *)lbl_80320F80) :
+                (u32)(lbl_80320F84->end - (u8 *)lbl_80320F84)
+        );
     }
-    return object;
+
+    return alloc;
 }
--- src/GameUtil/Mem.hpp
+++ src/GameUtil/Mem.hpp
@@ -10,9 +10,17 @@ enum EHeapMEM {
     eHeap_MEM2 = 1,
 };
 
+enum EHeapGroup {
+    eHeapGroup_Default = 0,
+    eHeapGroup_CommonAsset = 1, // allocate common assets (font, mesg, common cellanim & layout, etc.)
+    eHeapGroup_SceneAsset = 2,  // allocate scene assets (archives, etc.)
+    eHeapGroup_Scene = 3,       // allocate scene object
+};
+
 extern MEMiHeapHead *lbl_80320F80; // gHeapMEM1
 extern MEMiHeapHead *lbl_80320F84; // gHeapMEM2
 
+inline void *operator new(size_t, void *mem) { return mem; }
 void *operator new(size_t);
 void *operator new(size_t, EHeapMEM, s32);
 void *operator new[](size_t);
@@ -20,17 +28,15 @@ void *operator new[](size_t, EHeapMEM, s32);
 void operator delete(void *);
 void operator delete[](void *);
 
-inline void *operator new(size_t, void *p) { return p; }
-
 void fn_801D3564(void);
+void fn_801D3568(void);
 void fn_801D3634(void);
 void fn_801D3638(u32);
 u16 fn_801D363C(void);
 
 void fn_801D3644(void);
-void fn_801D369C(u16);
-void fn_801D3700(void *, MEMiHeapHead *, u32);
-void fn_801D375C(u32);
-void fn_801D3770(u32);
+void fn_801D369C(u16 groupID);
+void fn_801D375C(u16 groupID);
+void fn_801D3770(u16 groupID);
 
 #endif
